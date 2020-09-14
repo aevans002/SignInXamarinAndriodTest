d5282035417cebf79209fc978ef3c8375a8f4962
aevans002 <allanevans2005@msn.com>
2020-09-14 09:16:58 -0600

    Add files via upload

---
+++ SignInAppAndroid/SignInAppAndroid/CreationActivity.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace SignInAppAndroid
+{
+    [Activity(Label = "CreationActivity")]
+
+    //Normally add headers right here
+    public class CreationActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_creation);
+
+            TextView headerTxt = FindViewById<TextView>(Resource.Id.textView1);
+            EditText userTxtBx = FindViewById<EditText>(Resource.Id.userTxtBx);
+            EditText passwordTxtBx = FindViewById<EditText>(Resource.Id.passwordTxtBx);
+            Button createBtn = FindViewById<Button>(Resource.Id.createBtn);
+
+            createBtn.Click += CreateAccountClick;
+        }
+
+        private void CreateAccountClick(object sender, EventArgs eventArgs)
+        {
+            //Checking for issues with the entries
+            TextView headerTxt = FindViewById<TextView>(Resource.Id.textView1);
+            EditText userTxtBx = FindViewById<EditText>(Resource.Id.userTxtBx);
+            EditText passwordTxtBx = FindViewById<EditText>(Resource.Id.passwordTxtBx);
+            View view = (View)sender;
+            if (userTxtBx.Length() == 0)
+            {
+                ToastNotification.ToastMessage("Name too short");
+                return;
+            }
+
+            //Make things consistent for username and password, so they don't become so large they clip the list view
+            if (passwordTxtBx.Length() < 5 || userTxtBx.Length() < 5)
+            {
+                ToastNotification.ToastMessage("Password too short");
+                return;
+            }
+
+            if (passwordTxtBx.Length() > 12 || userTxtBx.Length() > 12)
+            {
+                ToastNotification.ToastMessage("Password too long");
+                return;
+            }
+
+            if (passwordTxtBx.Text.All(char.IsDigit))
+            {
+                ToastNotification.ToastMessage("Password needs a mix of characters");
+                return;
+            }
+
+            if (passwordTxtBx.Text.All(char.IsLetter))
+            {
+                ToastNotification.ToastMessage("Password needs a mix of characters");
+                return;
+            }
+
+            //Put password into char array to check for sequence
+            char[] charSplit = passwordTxtBx.Text.ToCharArray();
+            for (int i = 0; i < charSplit.Length; i++)
+            {   //Looking back I'm sure theres a better way to check for a sequence
+                    if (i < charSplit.Length - 3)
+                    {
+                        if (charSplit[i] == charSplit[i + 1] && charSplit[i + 1] == charSplit[i + 2]) //aaa
+                        {
+                            ToastNotification.ToastMessage("Password can't have repeated characters");
+                            return;
+                        }
+                    }
+                    if (i < charSplit.Length - 2)
+                    {
+                        if (charSplit[i] == charSplit[i + 1]) //aa
+                        {
+                            ToastNotification.ToastMessage("Password can't have repeated characters");
+                            return;
+                        }
+                    }
+
+                    if (i < charSplit.Length - 4)
+                    {
+                        if ((charSplit[i] + charSplit[i + 1]) == (charSplit[i + 2] + charSplit[i + 3])) //abab
+                        {
+                            ToastNotification.ToastMessage("Password can't have repeated characters");
+                            return;
+                        }
+                    }
+                    
+                    if (charSplit.Length >= 6 && i < charSplit.Length - 6)
+                    {
+                        if ((charSplit[i] + charSplit[i + 1] + charSplit[i + 2]) == (charSplit[i + 3] + charSplit[i + 4] + charSplit[i + 5])) //abcabc
+                        {
+                            ToastNotification.ToastMessage("Password can't have repeated characters");
+                            return;
+                        }
+                    }
+            }
+
+            SaveNewEntry(userTxtBx.Text, passwordTxtBx.Text);
+        }
+
+        private void SaveNewEntry(String user, String pass)
+        {
+            //Save entries to file, missing DB?!
+            //Confused a bit by why this needs System.IO
+            String newEntry = "";
+            if (System.IO.File.Exists(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/saved.txt"))
+            {
+                newEntry = System.IO.File.ReadAllText(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/saved.txt");
+                string[] oldArray = newEntry.Split(new string[] { "/r" }, StringSplitOptions.RemoveEmptyEntries);
+                for (int i = 0; i < oldArray.Length; i++)
+                {   //Need to add array of prohibited chars later
+                    string[] splitOld = oldArray[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    if (splitOld[0] == user)  //Can have same password but not user name
+                    {
+                        ToastNotification.ToastMessage("Entry already exists");  //Crude confirmation I know....
+                        return;
+                    }
+                }
+            }
+
+            newEntry += user + "," + pass + "/r";  //Put in check to prevent same entry multiple times
+            System.IO.File.WriteAllText(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/saved.txt", newEntry);
+            ToastNotification.ToastMessage("Entry created");
+        }
+
+        //Custom Toast notification
+        public static class ToastNotification
+        {
+            public static void ToastMessage(string message)
+            {
+                var context = Android.App.Application.Context;
+                var toastMessage = message;
+                var duration = ToastLength.Long;
+
+                //Make global class to add this in?
+                Toast.MakeText(context, toastMessage, duration).Show();
+            }
+        }
+    }
+}
---
+++ SignInAppAndroid/SignInAppAndroid/MainActivity.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.IO;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace SignInAppAndroid
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+
+    //Needs a header
+    public class MainActivity : AppCompatActivity
+    {
+        private ArrayAdapter<string> adapter;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            //link basic buttons and textboxes
+            ListView userListView = FindViewById<ListView>(Resource.Id.userListView);
+            Button newEntryBtn = FindViewById<Button>(Resource.Id.newEntryBtn);
+
+            newEntryBtn.Click += nextClick;
+            ReadEntries();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            //For refreshing the displayed listview
+            ReadEntries();
+        }
+
+
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+        private void ReadEntries()
+        {
+            //Reads saved file if it exists and populates listview
+            ListView userListView = FindViewById<ListView>(Resource.Id.userListView);
+            if (File.Exists(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/saved.txt"))
+            {
+                string existingEntries = File.ReadAllText(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + "/saved.txt");
+                string[] existingArray = existingEntries.Split(new string[] { "/r" }, StringSplitOptions.RemoveEmptyEntries);
+
+                for (int i = 0; i < existingArray.Length; i++)
+                {
+                    string[] splitEntry = existingArray[i].Split(new string[] { "," }, StringSplitOptions.RemoveEmptyEntries);
+                    existingArray[i] = "User:  " + splitEntry[0] + "  Password:  " + splitEntry[1];
+                }
+
+                adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, existingArray);
+                userListView.Adapter = adapter;
+            }
+            
+        }
+
+
+        private void nextClick(object sender, EventArgs eventArgs)
+        {
+            //Sends user to creation view
+            View view = (View)sender;
+            Intent nextActivity = new Intent(this, typeof(CreationActivity));
+            StartActivity(nextActivity);
+        }
+
+        //Generated code when initially creating the app
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+	}
+}
